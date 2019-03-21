480b0b5ae6778a3a11bb1081cb446139371c67b3
LiLiLiLaLa <37658628+LiLiLiLaLa@users.noreply.github.com>
2019-03-21 23:49:08 +0800

    Update Maze.h

--- Maze.h
+++ Maze.h
@@ -1,8 +1,10 @@
 #pragma once
 
+
 #include "Stack.h"
 #define N 6
 
+//迷宫的定义
 static int maze[N][N] = {
 	{ 0, 0, 0, 0, 0, 0 },
 	{ 0, 0, 1, 1, 1, 0 },
@@ -12,9 +14,12 @@ static int maze[N][N] = {
 	{ 0, 0, 1, 0, 0, 0 },
 };
 
+
+
 void MazePrint();
 int MazeGetPath(Pos entry);
 int MazeCheckIsAccess(Pos cur, Pos next);
 int MazeGetShortPath(Pos entry);
 int CheckAccess(Pos next);
 void TestMaze();
+
